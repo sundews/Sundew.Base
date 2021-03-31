5b02d815b39d107b9213df7371b399c085f64a88
Kim Hugener-Ohlsen <kim@hugener.dk>
2021-03-31 23:31:15 +0200

    Added Newline strings

--- Source/Sundew.Base.Text/Strings.cs
+++ Source/Sundew.Base.Text/Strings.cs
@@ -7,10 +7,27 @@
 
 namespace Sundew.Base.Text
 {
+    using System;
+
     /// <summary>Contains reusable strings.</summary>
     public static class Strings
     {
         /// <summary>A empty string.</summary>
         public const string Empty = "";
+
+        /// <summary>
+        /// The unix new line.
+        /// </summary>
+        public const string UnixNewLine = "\n";
+
+        /// <summary>
+        /// The windows new line.
+        /// </summary>
+        public const string WindowsNewLine = "\r\n";
+
+        /// <summary>
+        /// The environment new line.
+        /// </summary>
+        public static readonly string NewLine = Environment.NewLine;
     }
 }
