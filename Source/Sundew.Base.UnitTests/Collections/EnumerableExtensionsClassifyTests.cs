a0d81cadbfabc6f371d24eaf2134b7383e4ae994
Kim Hugener-Ohlsen <kim@hugener.dk>
2023-01-08 03:03:00 +0100

    Added more tests

--- Source/Sundew.Base.UnitTests/Collections/EnumerableExtensionsClassifyTests.cs
+++ Source/Sundew.Base.UnitTests/Collections/EnumerableExtensionsClassifyTests.cs
@@ -8,6 +8,7 @@
 namespace Sundew.Base.UnitTests.Collections;
 
 using System;
+using System.Linq;
 using FluentAssertions;
 using Sundew.Base.Collections;
 using Xunit;
@@ -42,5 +43,35 @@ public class EnumerableExtensionsClassifyTests
         var result = testee.Classify();
 
         result.Should().BeOfType<Multiple<int>>().Which.Items.Should().Equal(testee);
+    }
+
+    [Fact]
+    public void Classify_When_EnumerableYieldsNoItems_Then_ResultShouldBeEmpty()
+    {
+        var testee = new[] { 1, 2, 3 }.Where(x => x < 1);
+
+        var result = testee.Classify();
+
+        result.Should().BeOfType<Empty<int>>();
+    }
+
+    [Fact]
+    public void Classify_When_EnumerableHasSingleItem_Then_ResultShouldBeSingle()
+    {
+        var testee = new[] { 1, 2, 3 }.Where(x => x < 2);
+
+        var result = testee.Classify();
+
+        result.Should().BeOfType<Single<int>>().Which.Item.Should().Be(1);
+    }
+
+    [Fact]
+    public void Classify_When_EnumerableHasMultipleItems_Then_ResultShouldBeMultiple()
+    {
+        var testee = new[] { 1, 2, 3 }.Where(x => x < 3);
+
+        var result = testee.Classify();
+
+        result.Should().BeOfType<Multiple<int>>().Which.Items.Should().Equal(new[] { 1, 2 });
     }
 }
