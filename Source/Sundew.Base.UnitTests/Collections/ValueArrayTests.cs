de8069eba02753ac376b2b4494588c4f9ba19148
Kim Hugener-Ohlsen <kim@hugener.dk>
2023-03-23 23:22:32 +0100

    Added tests for Value* collections

---
+++ Source/Sundew.Base.UnitTests/Collections/ValueArrayTests.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ValueArrayTests.cs" company="Hukano">
+// Copyright (c) Hukano. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sundew.Base.UnitTests.Collections;
+
+using FluentAssertions;
+using Sundew.Base.Collections.Immutable;
+using Xunit;
+
+public class ValueArrayTests
+{
+    [Fact]
+    public void Equals_When_UsedWithInt_Then_LhsAndRhsShouldBeEqual()
+    {
+        ValueArray<int> lhs = System.Collections.Immutable.ImmutableArray.Create(4, 5, 6);
+        ValueArray<int> rhs = System.Collections.Immutable.ImmutableArray.Create(4, 5, 6);
+
+        ((object)lhs).Should().Be(rhs);
+    }
+}
---
+++ Source/Sundew.Base.UnitTests/Collections/ValueDictionaryTests.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ValueDictionaryTests.cs" company="Hukano">
+// Copyright (c) Hukano. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sundew.Base.UnitTests.Collections;
+
+using System.Collections.Immutable;
+using FluentAssertions;
+using Sundew.Base.Collections.Immutable;
+using Xunit;
+
+public class ValueDictionaryTests
+{
+    [Fact]
+    public void Equals_Then_LhsAndRhsShouldBeEqual()
+    {
+        ValueDictionary<int, int> lhs = ImmutableDictionary.Create<int, int>().Add(1, 2).Add(3, 4);
+        ValueDictionary<int, int> rhs = ImmutableDictionary.Create<int, int>().Add(1, 2).Add(3, 4);
+
+        ((object)lhs).Should().Be(rhs);
+    }
+}
