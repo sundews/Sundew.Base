e5e815609f7a4b4a60024e14377462e1d83b0d77
Kim Hugener-Ohlsen <kim@hugener.dk>
2023-12-08 06:37:40 +0100

    Fixed header filename

--- Source/Sundew.Base.Collections/Immutable/ImmutableListExtensions.cs
+++ Source/Sundew.Base.Collections/Immutable/ImmutableListExtensions.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ResultImmutableListExtensions.cs" company="Sundews">
+// <copyright file="ImmutableListExtensions.cs" company="Sundews">
 // Copyright (c) Sundews. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
