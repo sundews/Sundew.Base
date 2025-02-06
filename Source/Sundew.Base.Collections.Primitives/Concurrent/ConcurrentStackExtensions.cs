49eddf227c3542bb1949ff2983c74bdeecceca80
Kim Hugener-Ohlsen <kim@hugener.dk>
2025-02-06 03:32:12 +0100

    ConcurrentStack extensions

---
+++ Source/Sundew.Base.Collections.Primitives/Concurrent/ConcurrentStackExtensions.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConcurrentStackExtensions.cs" company="Sundews">
+// Copyright (c) Sundews. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sundew.Base.Collections.Concurrent;
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Extends concurrent stack with easy to use methods.
+/// </summary>
+public static class ConcurrentStackExtensions
+{
+    /// <summary>
+    /// Pops all items from the concurrent stack.
+    /// </summary>
+    /// <typeparam name="TItem">The item type.</typeparam>
+    /// <param name="concurrentStack">The concurrent stack.</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> containing the popped items.</returns>
+    public static IEnumerable<TItem> PopRange<TItem>(ConcurrentStack<TItem> concurrentStack)
+    {
+        var items = new TItem[concurrentStack.Count];
+        var count = concurrentStack.TryPopRange(items);
+        return items.Take(count);
+    }
+}
---
+++ Source/Sundew.Base.Disposal/ConcurrentStackExtensions.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConcurrentStackExtensions.cs" company="Sundews">
+// Copyright (c) Sundews. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sundew.Base.Disposal;
+
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Extends concurrent stack with easy to use methods.
+/// </summary>
+public static class ConcurrentStackExtensions
+{
+    /// <summary>
+    /// Pops all items from the concurrent stack.
+    /// </summary>
+    /// <typeparam name="TItem">The item type.</typeparam>
+    /// <param name="concurrentStack">The concurrent stack.</param>
+    public static void DisposeAll<TItem>(ConcurrentStack<TItem> concurrentStack)
+        where TItem : IDisposable
+    {
+        var items = new TItem[concurrentStack.Count];
+        var count = concurrentStack.TryPopRange(items);
+        for (int i = 0; i < count; i++)
+        {
+            items[i].Dispose();
+        }
+    }
+
+    /// <summary>
+    /// Pops all items from the concurrent stack.
+    /// </summary>
+    /// <typeparam name="TItem">The item type.</typeparam>
+    /// <param name="concurrentStack">The concurrent stack.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public static async Task DisposeAllAsync<TItem>(ConcurrentStack<TItem> concurrentStack)
+        where TItem : IAsyncDisposable
+    {
+        var items = new TItem[concurrentStack.Count];
+        var count = concurrentStack.TryPopRange(items);
+        for (int i = 0; i < count; i++)
+        {
+            await items[i].DisposeAsync().ConfigureAwait(false);
+        }
+    }
+}
