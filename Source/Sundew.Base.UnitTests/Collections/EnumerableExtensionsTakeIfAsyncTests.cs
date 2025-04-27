1daf667b306564853918f1392adb3233a0d22626
Kim Hugener-Ohlsen <kim@hugener.dk>
2025-04-27 14:33:04 +0200

    Improved tests

--- Source/Sundew.Base.UnitTests/Collections/EnumerableExtensionsTakeIfAsyncTests.cs
+++ Source/Sundew.Base.UnitTests/Collections/EnumerableExtensionsTakeIfAsyncTests.cs
@@ -28,7 +28,7 @@ public class EnumerableExtensionsTakeIfAsyncTests
     };
 
     private const long DefaultTimeoutMilliseconds = 100;
-    private const int TooLateAddDelay = 200;
+    private const int TooLateAddDelay = 300;
 
     [Theory]
     [InlineData(new int[0], null, -1, false, 0)]
