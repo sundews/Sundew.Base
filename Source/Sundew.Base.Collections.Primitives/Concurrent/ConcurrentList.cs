600d318377679c99558d9813f29eb30551a584e1
Kim Hugener-Ohlsen <kim@hugener.dk>
2025-04-27 14:17:20 +0200

    Fixed missing header

--- Source/Sundew.Base.Collections.Primitives/Concurrent/ConcurrentList.cs
+++ Source/Sundew.Base.Collections.Primitives/Concurrent/ConcurrentList.cs
@@ -1,4 +1,11 @@
-﻿namespace Sundew.Base.Collections.Concurrent;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConcurrentList.cs" company="Sundews">
+// Copyright (c) Sundews. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sundew.Base.Collections.Concurrent;
 
 using System.Collections;
 using System.Collections.Generic;
