b2140ebe3543c1a35ceb6297cea5f2ec406b1ca2
Kim Hugener-Ohlsen <kim@hugener.dk>
2025-11-16 03:01:06 +0100

    Added missing file header

--- Source/Sundew.Base.Collections/Parallelism.cs
+++ Source/Sundew.Base.Collections/Parallelism.cs
@@ -1,4 +1,11 @@
-﻿namespace Sundew.Base.Collections;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Parallelism.cs" company="Sundews">
+// Copyright (c) Sundews. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sundew.Base.Collections;
 
 using System;
 using System.Threading.Tasks;
